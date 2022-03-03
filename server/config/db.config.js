62b12b17d480ddd249bd3e52c4303796df288856
pvdthien310 <71278418+pvdthien310@users.noreply.github.com>
2022-03-03 09:15:41 +0700

    fix config.database

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-    HOST: "https://dashboard.heroku.com/",
-    USER: "ovzjvxulraxgun",
-    PASSWORD: "6a7a21088573a0da49ea11793a6381f1311a0e70552dbe553c2bfeaf598b9fea",
-    DB: "d1bivukjltv8os",
-    dialect: "Comebuy",
+    host: "ec2-54-156-110-139.compute-1.amazonaws.com",
+    user: "ovzjvxulraxgun",
+    password: "6a7a21088573a0da49ea11793a6381f1311a0e70552dbe553c2bfeaf598b9fea",
+    database: "d1bivukjltv8os",
+    dialect: "postgres",
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION
+      }
     }
   };
